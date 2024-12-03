79dc103f95036c745b6e7f55bb107bd28d02c7c0
DorMaj <fejtgame@gmail.com>
2024-12-03 01:43:05 +0100

    Implementacja zmiennych oraz interakcji z przyciskami

--- app/src/main/java/com/example/pdietetyczny/MainActivity.kt
+++ app/src/main/java/com/example/pdietetyczny/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.example.pdietetyczny
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.pdietetyczny.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var click = 0
+    private var textsize = 20
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.myButton.setOnClickListener{
+            click += 1
+            if (textsize<200) {
+                textsize += 5
+            }
+            binding.myText.textSize = textsize.toFloat()
+            binding.myText.text = click.toString()
         }
+
+
+        //binding.myButton.setOnClickListener{
+        //    binding.myText.text = "zmiana!"
+        //}
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Stan aplikacji", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Stan aplikacji", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Stan aplikacji", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Stan aplikacji", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Stan aplikacji", "onDestroy")
     }
 }
